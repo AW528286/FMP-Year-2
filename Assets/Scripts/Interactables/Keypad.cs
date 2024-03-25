9b6992dfb50dd82fdd5e89a8d297c07e2e68ea7f
AW528286 <113694026+AW528286@users.noreply.github.com>
2024-03-25 10:22:03 +0000

    Animation trial and simple prison setup done

--- Assets/Scripts/Interactables/Keypad.cs
+++ Assets/Scripts/Interactables/Keypad.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drawer : Interactable
+public class Keypad : Interactable
 {
     [SerializeField]
-    private GameObject drawer;
-    private bool drawertestopen;
+    private GameObject door;
+    private bool doorOpen;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     protected override void Interact()
     {
-        drawertestopen = !drawertestopen;
-        drawer.GetComponent<Animator>().SetBool("IsOpen", drawertestopen);
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
     }
 }
