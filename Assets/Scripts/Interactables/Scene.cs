f8095992319cf356e37ace2e697a752dcb444ee5
AW528286 <113694026+AW528286@users.noreply.github.com>
2024-05-21 15:20:42 +0100

    Outsied Working and way to get to all scenes through game

---
+++ Assets/Scripts/Interactables/Scene.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // Name of the scene to load
+    public string EscapeOutside;
+
+    // This method is called when the collider enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug log to check if collision is detected
+        Debug.Log("Collision detected with: " + other.name);
+
+        // Check if the colliding object has the tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Debug log to confirm player tag is detected
+            Debug.Log("Player detected, loading scene: " +EscapeOutside);
+
+            // Load the specified scene
+            SceneManager.LoadScene(EscapeOutside);
+        }
+    }
+}
